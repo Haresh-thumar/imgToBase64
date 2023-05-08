a3a122875856cc1ebe80a65e02a76b1e8e6b3817
Haresh-thumar <hareshthumar46@gmail.com>
2023-05-08 21:44:22 +0530

    complete url shortner

--- src/app/url-shortner/url-shortner.component.ts
+++ src/app/url-shortner/url-shortner.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgTinyUrlService} from 'ng-tiny-url';
+import { NgTinyUrlService } from 'ng-tiny-url';
 
 @Component({
   selector: 'app-url-shortner',
@@ -10,32 +10,52 @@ export class UrlShortnerComponent {
 
   model = {
     inputUrl: ''
-  }
+  };
 
-  isLoading:boolean = false;
+  isLoading: boolean = false;
   shortenedUrl = '';
-  isTextCopied:boolean = false;
+  isTextCopied: boolean = false;
 
-  constructor(private tinyUrl: NgTinyUrlService){}
+  constructor(private tinyUrl: NgTinyUrlService) { }
 
-  onSubmitUrl(urlForm:any){
-    if(urlForm.valid) {
+  // create Url
+  onSubmitUrl(urlForm: any) {
+    if (urlForm.valid) {
       this.isLoading = true;
       this.tinyUrl.shorten(this.model.inputUrl).subscribe(res => {
         this.shortenedUrl = res;
-        this.isLoading = false
-      },
-      (error) => {
-        alert('Something Went Wrong, Please Check Your URL and Try Again...')
         this.isLoading = false;
-      })
+      },
+        (error) => {
+          alert('Something Went Wrong, Please Check Your URL and Try Again...');
+          this.isLoading = false;
+        });
     }
   }
 
-
-  reset(){
+  // Reset Form
+  reset() {
     this.model.inputUrl = '';
-    this.isTextCopied = false
+    this.isTextCopied = false;
+  }
+
+  // Copy Url
+  copyUrl(shortUrlElementRef: any) {
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'text');
+    inputElement.setAttribute('value', shortUrlElementRef.innerHTML);
+    inputElement.select();
+    inputElement.setSelectionRange(0, 999999);
+    try {
+      navigator.clipboard.writeText(inputElement.value);
+      this.isTextCopied = true;
+      setTimeout(() => {
+        this.isTextCopied = true;
+      }, 2000);
+    }
+    catch (e) {
+      console.log('Error While Coping....', e);
+    }
   }
 
 
